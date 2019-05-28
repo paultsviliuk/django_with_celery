139d8ae9e5a4197df2221fa9457209e0b4b01da8
angmar33 <paultsvilyuk@gmail.com>
2019-05-29 00:28:20 +0300

    verification with celery added.

--- quick_publisher/main/models.py
+++ quick_publisher/main/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db.models import signals
+from django.core.mail import send_mail
+from .tasks import send_verification_email
+
+import uuid
 
 
 class UserAccountManager(BaseUserManager):
@@ -38,6 +43,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     full_name = models.CharField('full name', blank=True, null=True, max_length=400)
     is_staff = models.BooleanField('staff status', default=False)
     is_active = models.BooleanField('active', default=True)
+    is_verified = models.BooleanField('verified', default=False)  # Add the `is_verified` flag
+    verification_uuid = models.UUIDField('Unique Verification UUID', default=uuid.uuid4)
 
     def get_short_name(self):
         return self.email
@@ -48,3 +55,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     # def __unicode__(self):
     def __str__(self):
         return self.email
+
+
+def user_post_save(sender, instance, signal, *args, **kwargs):
+    if not instance.is_verified:
+        # Send verification email
+        # Send verification email
+        send_verification_email.delay(instance.pk)
+
+
+signals.post_save.connect(user_post_save, sender=User)
--- quick_publisher/quick_publisher/urls.py
+++ quick_publisher/quick_publisher/urls.py
@@ -17,10 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from publish.views import view_post
+from main.views import home, verify
 
 
 urlpatterns = [
+    url(r'^$', home, name='home'),
     url(r'^admin/', admin.site.urls),
+    url(r'^verify/(?P<uuid>[a-z0-9\-]+)/', verify, name='verify'),
     url(r'^(?P<slug>[a-zA-Z0-9\-]+)', view_post, name='view_post')
 ]
 
